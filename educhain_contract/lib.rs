8400238d1fed5ff52cb22c8bb67b857aa79efac3
Facundo Alejandro Caniza <facundocaniza@proton.me>
2026-01-13 21:40:45 -0300

    add admin skeleton

--- educhain_contract/lib.rs
+++ educhain_contract/lib.rs
@@ -8,6 +8,7 @@ mod educhain_contract {
     pub enum Role {
         Owner,
         Admin,
+        Public,
     }
 
     #[ink::storage_item(packed)]
@@ -16,29 +17,51 @@ mod educhain_contract {
         Revoked,
     }
 
+    #[ink::storage_item(packed)]
     pub struct Certificate {
         id: u64,
         studen_address: Address,
         course_name: String,
         issue_date: Timestamp,
         state: CertificateState,
+        certificate_hash: Hash,
     }
+
+    #[ink(event)]
+    pub struct AdminAddedEvent {
+        #[ink(topic)]
+        admin_added: Option<Address>,
+        #[ink(topic)]
+        owner: Option<Address>,
+    }
+
+    // pub struct
 
     #[ink(storage)]
     pub struct Educhain {
         roles: Mapping<Address, Role>,
-        
-
+        students_certificates: Mapping<Address, Certificate>,
     }
     impl Educhain {
         #[ink(constructor)]
         pub fn new() -> Self {
             let owner = Self::env().caller();
-            let mut mapping = Mapping::new();
-            mapping.insert(owner, &Role::Owner);
-            Self { roles: mapping }
+            let mut roles = Mapping::new();
+            roles.insert(owner, &Role::Owner);
+            let students_certificates = Mapping::new();
+            Self::env().emit_event(AdminAddedEvent {
+                admin_added: Some(owner),
+                owner: Some(owner),
+            });
+            Self {
+                roles,
+                students_certificates,
+            }
         }
         #[ink(message)]
-        pub fn hello(&self) {}
+        pub fn add_admin(&mut self, admin_added: Address) {
+            let mut roles = &self.roles;
+            todo!()
+        }
     }
 }
